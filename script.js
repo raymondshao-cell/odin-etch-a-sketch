36470d3444ca82bb70c5524bd72b3bca84421bfb
Raymond Shao <243028283+raymondshao-cell@users.noreply.github.com>
2025-11-22 20:04:19 +1100

    Added a button that can dynamically change the size of the grid with a max size of 100 x 100

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-const promptBtn = document.createElement('button');
+let gridHeight = 4;
 
-numSquares = 16;
+function createGrid(gH) {
+    const existingContainer = document.querySelector('.container');
+    if (existingContainer) {
+        existingContainer.remove();
+    }
 
-const container = document.querySelector('.container');
+    const container = document.createElement('div');
+    container.classList.add('container');
+    document.body.appendChild(container);
 
-for (let i = 0; i < numSquares; i++) {
+    calcWidth = `calc(${100 / gH}% - (2 * var(--gmargin)))`
+    console.log(calcWidth);
+
+    document.documentElement.style.setProperty('--calcWidth', calcWidth);
+
+
+    for (let i = 0; i < gH ** 2; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
     }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.addEventListener('mouseover', function() {
-        if (this.style.backgroundColor === "") {
-            this.style.backgroundColor = 'black';
-        }
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', function() {
+            if (this.style.backgroundColor === "") {
+                this.style.backgroundColor = 'black';
+            }
+        });
     });
+}
+
+const promptBtn = document.createElement('button');
+promptBtn.textContent = 'Change Grid-Size';
+promptBtn.classList.add('size-button');
+document.body.appendChild(promptBtn);
+
+createGrid(gridHeight);
+
+promptBtn.addEventListener('click', () => {
+    let inputNum = prompt("Enter a number for the grid's size: ");
+    if (inputNum !== null) {
+        gH = parseInt(inputNum, 10);
+    } else {
+        return
+    }
+
+    if (gH > 100) {
+        gH = 100;
+        console.log(`Size too large, set to max size: ${gH}`);
+    }
+
+    createGrid(gH);
 });
